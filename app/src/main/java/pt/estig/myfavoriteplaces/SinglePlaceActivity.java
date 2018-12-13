569adb4cc6c14354f897014d85a9a8dba063bce8
Catalin Cicau <catalin.cicau@gmail.com>
2018-12-13 16:10:16 +0000

    uodate de hoje!

--- app/src/main/java/pt/estig/myfavoriteplaces/SinglePlaceActivity.java
+++ app/src/main/java/pt/estig/myfavoriteplaces/SinglePlaceActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -61,5 +62,9 @@ public class SinglePlaceActivity extends AppCompatActivity {
         ByteArrayInputStream arrayInputStream = new ByteArrayInputStream(photoBytes);
         Bitmap bitmap = BitmapFactory.decodeStream(arrayInputStream);
         return bitmap;
+    }
+
+    public void btnBackOnClick(View view) {
+        finish();
     }
 }
