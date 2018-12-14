e62c6a4b1751c08b06bdc3013bc08838c4ea75b9
Catalin Cicau <catalin.cicau@gmail.com>
2018-12-14 17:20:43 +0000

    correção da act map

--- app/src/main/java/pt/estig/myfavoriteplaces/PlaceOnMapActivity.java
+++ app/src/main/java/pt/estig/myfavoriteplaces/PlaceOnMapActivity.java
@@ -1,5 +1,6 @@
 package pt.estig.myfavoriteplaces;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -16,8 +17,11 @@ public class PlaceOnMapActivity extends FragmentActivity implements OnMapReadyCa
     private double lat;
     private double lng;
     private String place_name;
+    private String place_description;
+    byte[] place_photo;
+    Intent intent;
+
     float zoom = 12.0f;
-    private LatLng myplace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,8 @@ public class PlaceOnMapActivity extends FragmentActivity implements OnMapReadyCa
         lat = getIntent().getDoubleExtra("PLACE_LAT",0);
         lng = getIntent().getDoubleExtra("PLACE_LNG", 0);
         place_name = getIntent().getStringExtra("PLACE_NAME");
+        //place_photo = getIntent().getByteArrayExtra("PLACE_PHOTO");
+        place_description = getIntent().getStringExtra("PLACE_DESCRIPTION");
 
         setContentView(R.layout.activity_place_on_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -33,6 +39,15 @@ public class PlaceOnMapActivity extends FragmentActivity implements OnMapReadyCa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        /*intent.putExtra("PLACE_LAT", lat);
+        intent.putExtra("PLACE_LNG", lng);
+        intent.putExtra("PLACE_NAME", place_name);
+        intent.putExtra("PLACE_PHOTO", place_photo);
+        intent.putExtra("PLACE_DESCRIPTION", place_description);
+    */
+
     }
 
     @Override
@@ -53,7 +68,9 @@ public class PlaceOnMapActivity extends FragmentActivity implements OnMapReadyCa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        myplace = new LatLng(lat, lng);
+
+        // Add a marker in Sydney and move the camera
+        LatLng myplace = new LatLng(lat, lng);
         mMap.addMarker(new MarkerOptions().position(myplace).title(place_name));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myplace, zoom));
     }
